b5dd4e8febcc6ddb1a41279a24f12a45a36a34f2
DSPMSD78 <prasad.duggirala.369@gmail.com>
2023-07-12 14:17:02 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,44 @@
+import {
+  update as updateSnake,
+  draw as drawSnake,
+  SNAKE_SPEED,
+  getSnakeHead,
+  snakeIntersection,
+} from "./snake.js";
+import { update as updateFood, draw as drawFood } from "./food.js";
+import { outsideGrid } from "./grid.js";
+
+let lastRenderTime = 0;
+const gameBoard = document.getElementById("game-board");
+let gameOver = false;
+
+function main(currentTime) {
+  if (gameOver) {
+    return alert("You Lose");
+  }
+  window.requestAnimationFrame(main);
+  const secsSinceLastRender = (currentTime - lastRenderTime) / 1000;
+  if (secsSinceLastRender < 1 / SNAKE_SPEED) return;
+
+  lastRenderTime = currentTime;
+
+  update();
+  draw();
+}
+window.requestAnimationFrame(main);
+
+function update() {
+  updateSnake();
+  updateFood();
+  checkDeath();
+}
+
+function draw() {
+  gameBoard.innerHTML = "";
+  drawSnake(gameBoard);
+  drawFood(gameBoard);
+}
+
+function checkDeath() {
+  gameOver = outsideGrid(getSnakeHead()) || snakeIntersection();
+}
